6cf46fe5513f2c5e6ff96a57b4be8c7e32543cbe
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-05 14:09:06 -0300

    more information about os module 05/09/2022

---
+++ moduloOS/app.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+// Tipo sistema
+// console.log(os.type())
+
+//Ver directorio principal
+// console.log(os.homedir())
+
+//Numero de segundos desde que se inicio el equipo
+// console.log(os.uptime())
+
+//Informacion del usuario
+// console.log(os.userInfo())
