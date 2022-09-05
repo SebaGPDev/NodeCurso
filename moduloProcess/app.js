12e38e6010d845ee45217fd1fb9afb1fed37e546
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-05 13:57:31 -0300

    interesting things with process module 05/09/2022

--- moduloProcess/app.js
+++ moduloProcess/app.js
@@ -1,1 +1,16 @@
-console.log(process);
+// console.log(process);
+// console.log(process.env);//Puedo entrar a las propiedades del modulo
+
+// console.log(process.argv);
+//Gracias a argv [node, app.js, .., ..,]
+
+//Tambien se puede hacer esto
+// console.log(process.argv[4]);
+
+//Tambien 
+// for (let i = 2; i < process.argv.length; i++) {
+//     console.log(process.argv[i]);
+// }
+
+//Saber la memoria en uso
+// console.log(process.memoryUsage());
