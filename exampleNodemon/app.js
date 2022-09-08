0e8bb3080e46d904bba9f6c23853be7d62d741ac
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-08 09:07:23 -0300

    testing environment variables 08/09/2022

--- exampleNodemon/app.js
+++ exampleNodemon/app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.end("Nodemon Funciona");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Servidor listening on http://localhost:3000");
 });
