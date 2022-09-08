c1c1e6c767b3140f80290c2d8696b69c299a840c
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-08 19:45:09 -0300

    finished examples express 08/09/2022

---
+++ cursoExpress/cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+  programacion: [
+    {
+      id: 1,
+      titulo: "Aprende Python",
+      lenguaje: "Python",
+      vistas: 1300,
+      nivel: "Basico",
+    },
+    {
+      id: 2,
+      titulo: "Python Intermedio",
+      lenguaje: "Python",
+      vistas: 13000,
+      nivel: "Intermedio",
+    },
+    {
+      id: 3,
+      titulo: "Aprende JavaScript",
+      lenguaje: "JavaScript",
+      vistas: 1200,
+      nivel: "Basico",
+    },
+  ],
+  Matematicas: [
+    {
+      id: 1,
+      titulo: "Aprende Calculo",
+      tema: "Calculo",
+      vistas: 1300,
+      nivel: "Basico",
+    },
+    {
+      id: 2,
+      titulo: "Aprende Algebra",
+      tema: "Algebra",
+      vistas: 11000,
+      nivel: "Intermedio",
+    },
+  ],
+};
+
+module.exports.infoCursos = infoCursos;
