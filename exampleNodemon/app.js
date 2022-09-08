2d08bea1b053f26f29f8234c42bbc2cba7759b71
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-08 06:51:56 -0300

    installing and testing nodemon module 08/09/2022

---
+++ exampleNodemon/app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Nodemon Funciona");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("Servidor listening on http://localhost:3000");
+});
