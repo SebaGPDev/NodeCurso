25fac05cc6af5994a37e6066d96639ce476084f1
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-09 19:31:59 -0300

    working with routes 09/09/2022

--- cursoExpress/app.js
+++ cursoExpress/app.js
@@ -1,6 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const {infoCursos} = require('./cursos.js')
+const { infoCursos } = require("./cursos.js");
 
-console.log(infoCursos)
+// console.log(infoCursos)
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/cursos", (req, res) => {
+  res.send(JSON.stringify(infoCursos));
+});
+app.get("/api/cursos/programacion", (req, res) => {
+  res.send(JSON.stringify(infoCursos.Programacion));
+});
+app.get("/api/cursos/matematica", (req, res) => {
+  res.send(JSON.stringify(infoCursos.Matematicas));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
+});
