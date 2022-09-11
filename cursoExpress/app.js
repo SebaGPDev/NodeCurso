3ae8e00ff039b64db7926a2549bfa2aab763dd66
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-11 19:08:34 -0300

    Learning route parameters 11/09/2022

--- cursoExpress/app.js
+++ cursoExpress/app.js
@@ -19,6 +19,43 @@ app.get("/api/cursos/matematica", (req, res) => {
   res.send(JSON.stringify(infoCursos.Matematicas));
 });
 
+// app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
+//   const lenguaje = req.params.lenguaje;
+//   const resultado = infoCursos.Programacion.filter(
+//     (programacion) => programacion.lenguaje === lenguaje
+//   );
+//   if (resultado.length === 0) {
+//     return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+//   }
+//   res.send(JSON.stringify(resultado));
+// });
+
+app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
+  const lenguaje = req.params.lenguaje;
+  const resultados = infoCursos.Programacion.filter(
+    (curso) => curso.lenguaje === lenguaje
+  );
+
+  if (resultados.length === 0) {
+    return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+  }
+
+  res.send(JSON.stringify(resultados));
+});
+
+app.get("/api/cursos/matematicas/:tema", (req, res) => {
+  const tema = req.params.tema;
+  const resultados = infoCursos.Matematicas.filter(
+    (curso) => curso.tema === tema
+  );
+
+  if (resultados.length === 0) {
+    return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+  }
+
+  res.send(JSON.stringify(resultados));
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
