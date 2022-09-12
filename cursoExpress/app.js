f250cb1986349362e483431b72f1f483acd61b45
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-11 21:34:54 -0300

    Learning query parameters 11/09/2022

--- cursoExpress/app.js
+++ cursoExpress/app.js
@@ -19,17 +19,6 @@ app.get("/api/cursos/matematica", (req, res) => {
   res.send(JSON.stringify(infoCursos.Matematicas));
 });
 
-// app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
-//   const lenguaje = req.params.lenguaje;
-//   const resultado = infoCursos.Programacion.filter(
-//     (programacion) => programacion.lenguaje === lenguaje
-//   );
-//   if (resultado.length === 0) {
-//     return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
-//   }
-//   res.send(JSON.stringify(resultado));
-// });
-
 app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
   const lenguaje = req.params.lenguaje;
   const resultados = infoCursos.Programacion.filter(
@@ -39,10 +28,43 @@ app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
   if (resultados.length === 0) {
     return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
   }
-
+  if (req.query.ordenar === "vistas") {
+    return res.send(
+      JSON.stringify(resultados.sort((a, b) => b.vistas - a.vistas))
+      //http://localhost:3000/api/cursos/programacion/python?ordenar=vistas
+    );
+  }
   res.send(JSON.stringify(resultados));
 });
 
+// app.get("api/cursos/programacion/:lenguaje/:nivel", (req, res) => {
+//   const lenguaje = req.params.lenguaje;
+//   const nivel = req.params.nivel;
+//   const resultados = infoCursos.Programacion.filter(
+//     (curso) => curso.lenguaje === lenguaje && curso.nivel === nivel
+//   );
+
+//   if (resultados.length === 0) {
+//     return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+//   }
+//   res.send(JSON.stringify(resultados));
+// });
+app.get("/api/cursos/programacion/:lenguaje/:nivel", (req, res) => {
+  const lenguaje = req.params.lenguaje;
+  const nivel = req.params.nivel;
+
+  const resultados = infoCursos.Programacion.filter(
+    (curso) => curso.lenguaje === lenguaje && curso.nivel === nivel
+  );
+
+  if (resultados.length === 0) {
+    return res
+      .status(404)
+      .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+  }
+
+  res.send(JSON.stringify(resultados));
+});
 app.get("/api/cursos/matematicas/:tema", (req, res) => {
   const tema = req.params.tema;
   const resultados = infoCursos.Matematicas.filter(
